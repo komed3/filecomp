2bca8df5800dd76481e08dbe8923dea68c51ae8a
komed3 <pk97@outlook.de>
2025-06-30 20:33:28 +0200

    Update tui.sh

--- src/tui.sh
+++ src/tui.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
+# Max. colums
+MAX_COLS=$( tput cols )
+MAX_ROWS=$( tput lines )
+
+# Init the screen for filecomp
 screen_init() {
     tput clear
     tput civis
 }
 
+# Bring the screen back to normal
 screen_clear() {
     tput cnorm
     tput srg0
     clear
+}
+
+header() {
+    tput cup 0 0
+    printf "FILECOMP"
+}
+
+footer() {
+
 }
