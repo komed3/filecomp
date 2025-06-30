c57c0bfe092ebe3a9ae36a1fe0787f88a4ec20ed
komed3 <pk97@outlook.de>
2025-06-30 23:45:02 +0200

    add some comments

--- src/hash.sh
+++ src/hash.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+# Get the files SHA1 hash
 hash_sha1() {
     local file="$1"
     sha1sum "$file" | awk '{print $1}'
 }
 
+# Get the files SHA256 hash
 hash_sha256() {
     local file="$1"
     sha256sum "$file" | awk '{print $1}'
 }
 
+# Get the files MD5 hash
 hash_md5() {
     local file="$1"
     md5sum "$file" | awk '{print $1}'
