487746c20f89fdbc6296b8c07a26e5da9860f730
komed3 <pk97@outlook.de>
2025-07-01 09:58:42 +0200

    create colors utility

---
+++ src/utils/colors.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+export   BLACK="$(tput setaf 0)"
+export     RED="$(tput setaf 1)"
+export   GREEN="$(tput setaf 2)"
+export  YELLOW="$(tput setaf 3)"
+export    BLUE="$(tput setaf 4)"
+export MAGENTA="$(tput setaf 5)"
+export    CYAN="$(tput setaf 6)"
+export   WHITE="$(tput setaf 7)"
+
+export   BG_BLACK="$(tput setab 0)"
+export     BG_RED="$(tput setab 1)"
+export   BG_GREEN="$(tput setab 2)"
+export  BG_YELLOW="$(tput setab 3)"
+export    BG_BLUE="$(tput setab 4)"
+export BG_MAGENTA="$(tput setab 5)"
+export    BG_CYAN="$(tput setab 6)"
+export   BG_WHITE="$(tput setab 7)"
+
+reset_color () {
+    tput sgr0
+}
+
+set_color () {
+    local color="$1"
+    tput setaf $color
+}
+
+set_bgcol () {
+    local color="$1"
+    tput setab $color
+}
