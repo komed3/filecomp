a9f2ebe45e49712c5095a447084c548065d7b440
komed3 <pk97@outlook.de>
2025-07-02 01:10:27 +0200

    refactor folder selection script / add cust. title support

--- src/utils/folder.sh
+++ src/utils/folder.sh
@@ -2,6 +2,7 @@
 
 # --------------------------------------------------------------------------------
 # src/utils/folder.sh
+# Interactive Folder Selection
 #
 # Directory navigator with arrow key control, similar to Midnight Commander:
 # -> up/down = navigation
@@ -12,94 +13,112 @@
 source ./src/utils/colors.sh
 source ./src/utils/tui.sh
 
-select_folder() {
+select_folder () {
 
-    local begin=$(( START + 2 ))
-    local current_path="$1"
+    # Set up folder selection
+    local title="$1"
+    local current=$(( START + 1 ))
+    local begin=$(( START + 3 ))
+    local current_path="$2"
     local entries
 
     local pointer=0
     local key
 
-    print_actions "[⇕] Move" "[⇔] Navigate" "[↵] Confirm" 2 "[Q] Quit"
+    # Clear previous content and print title
+    clear_content; print_title "$title"
 
-    clear_screen
-    print_title "SELECT FOLDER"
+    # Print options / menu actions
+    print_actions "[⇕] Navigate" "[⇔] Change directory" "[↵] Confirm" 2 "[Q] Quit"
 
     # The main loop
     # Show list of directories
     while true; do
 
-        # Inhalt des Ordners einlesen (nur Verzeichnisse)
+        # Jump to content
+        jump_content
+
+        # Print the current path
+        tput cup $current 0; tput el
+        printf "%sPath: %s%s" "$PRFX" "${YELLOW}" "$current_path"
+        reset_color
+
+        # Read contents of the folder (directories only)
         mapfile -t entries < <(find "$current_path" -mindepth 1 -maxdepth 1 -type d | sort)
         local visible_count=$(( END - begin ))
 
-        # Startposition der Anzeige berechnen (für Scroll-Effekt)
+        # Calculate start position of the display (for scroll effect)
         local offset=0
         (( pointer >= visible_count )) && offset=$(( pointer - visible_count + 1 ))
 
-        # Sichtbare Einträge anzeigen
+        # Show visible entries
         for (( i=0; i < visible_count; i++ )); do
 
             local index=$(( i + offset ))
             local line=$(( begin + i ))
 
+            tput cup $line 0; tput el
+
             [[ $index -ge ${#entries[@]} ]] && break
 
-            local name="$(basename "${entries[$index]}")"
-
-            tput cup $line 0
-            tput el
+            local name="$( basename "${entries[$index]}" )"
 
             hl=""
 
-            if (( index == pointer )); then
-                hl="${BLACK}${BG_WHITE}"
-            fi
+            if (( index == pointer )); then hl="${REV}"; fi
 
             printf "%s%s%s" "$PRFX" "$hl" "$name"
             reset_color
 
         done
 
-        # Überschüssige alte Zeilen löschen
+        # Delete old lines surplus to demand
         for (( i=${#entries[@]} - offset; i < visible_count; i++ )); do
             local line=$(( begin + i ))
             tput cup $line 0; tput el
         done
 
-        # Eingabe lesen
+        # Read key input (with escape for arrow keys)
         IFS= read -rsn1 key
+
         if [[ $key == $'\x1b' ]]; then
             read -rsn2 rest
             key+="$rest"
         fi
 
+        # Parse key input
         case "$key" in
 
-            $'\x1b[A')  # Hoch
-                (( pointer > 0 )) && ((pointer--))
+            # Navigate upwards
+            $'\x1b[A')
+                (( pointer > 0 )) && (( pointer-- ))
                 ;;
 
-            $'\x1b[B')  # Runter
-                (( pointer < ${#entries[@]} - 1 )) && ((pointer++))
+            # Navigate down
+            $'\x1b[B')
+                (( pointer < ${#entries[@]} - 1 )) && (( pointer++ ))
                 ;;
 
-            $'\x1b[C')  # Rechts (tiefer)
+            # Go deeper
+            $'\x1b[C')
                 current_path="${entries[$pointer]}"
                 pointer=0
                 ;;
 
-            $'\x1b[D')  # Links (zurück)
-                current_path="$(dirname "$current_path")"
+            # Come back a level
+            $'\x1b[D')
+                current_path="$( dirname "$current_path" )"
                 pointer=0
                 ;;
 
-            "")  # Enter
-                echo "$current_path"
-                return 0
+            # Enter: get back the current (selected) directory
+            # If within deepest directory, return current one
+            "")
+                result="${entries[$pointer]:-$current_path}"
+                break
                 ;;
 
+            # Quit program
             [qQ])
                 quit
                 ;;
