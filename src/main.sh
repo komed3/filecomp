3fc573b0c551f55c32755bb7f5c2b25f284c9216
komed3 <pk97@outlook.de>
2025-07-02 23:51:04 +0200

    add feature to read inputs and improve the main loop

--- src/main.sh
+++ src/main.sh
@@ -3,10 +3,19 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 source "$SCRIPT_DIR/utils/ui.sh"
+source "$SCRIPT_DIR/utils/key.sh"
 
 main () {
 
     setup_env
+
+    while true; do
+
+        KEY=$( read_key )
+
+        echo "$KEY"
+
+    done
 
     reset_env
 
---
+++ src/utils/key.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+read_key () {
+
+    local key rest seq
+
+    # Read raw terminal inputs
+    IFS= read -rsn1 key 2>/dev/null || return
+
+    # Escape sequence (arrow, function keys)
+    if [[ "$key" == $'\e' ]]; then
+
+        # Read next 2 characters with timeout (for escape sequences)
+        IFS= read -rsn1 -t 0.005 rest 2>/dev/null
+        key+="$rest"
+        IFS= read -rsn1 -t 0.001 seq 2>/dev/null
+        key+="$seq"
+
+        # Parse special keys
+        case "$key" in
+            $'\e[A' ) echo "arrow_up" ;;
+            $'\e[B' ) echo "arrow_down" ;;
+            $'\e[C' ) echo "arrow_right" ;;
+            $'\e[D' ) echo "arrow_left" ;;
+            $'\e[5' ) echo "page_up" ;;
+            $'\e[6' ) echo "page_down" ;;
+            * )       echo 0 ;;
+        esac
+
+    else
+
+        # Handle regular keys
+        # Convert control keys to readable names
+        case "$key" in
+            $' ' )           echo "space" ;;
+            $'\n'|$'\r'|"" ) echo "enter" ;;
+            $'\b'|$'\x7f' )  echo "backspace" ;;
+            $'\t' )          echo "tab" ;;
+            [[:alnum:]] )    echo $key ;;
+            * )              echo 0 ;;
+        esac
+
+    fi
+
+    # Catch all following inputs (e.g. if the button is held down)
+    while IFS= read -rsn1 -t 0.001 _; do :; done
+
+}
