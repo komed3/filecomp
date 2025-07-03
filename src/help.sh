262c0cf1ada70ebc3b57554c3c903e0d09382292
komed3 <pk97@outlook.de>
2025-07-03 23:59:47 +0200

    add help page text

--- src/help.sh
+++ src/help.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+source "$SCRIPT_DIR/utils/colors.sh"
 source "$SCRIPT_DIR/utils/ui.sh"
 
 print_help () {
@@ -7,6 +8,24 @@ print_help () {
     print_actions
 
     print_title "WELCOME TO FILECOMP"
+
+    local help=(
+        "FileComp recursively compares two directories for unique files using their hash values."
+        ""
+        "${BOLD}Steps:${RESET}"
+        "1) Select base directory."
+        "2) Select directory to compare."
+        "3) Choose hash algorithm (e.g., SHA1)."
+        "4) Choose output: log, copy, or both."
+        "5) (Optional) Select folder to copy unique files."
+        "6) Decide whether to keep or delete hash DB."
+        ""
+        "Use ${BOLD}arrow keys${RESET} and ${BOLD}Enter${RESET} to navigate menus."
+        "Press ${BOLD}Q${RESET} anytime to quit."
+    )
+
+    for i in "${!help[@]}"; do printf "%s%s\n" "$PRFX" "${help[i]}"; done
+
 
     clear_content
 
