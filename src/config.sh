642c50a01ae6a3d0fe453a753ff00db6da49c247
komed3 <pk97@outlook.de>
2025-07-04 21:27:03 +0200

    first test for config.

--- src/config.sh
+++ src/config.sh
@@ -1,7 +1,32 @@
 #!/bin/bash
 
+source "$SCRIPT_DIR/utils/ctrl.sh"
+source "$SCRIPT_DIR/utils/ui.sh"
+
+BASE_DIR=""
+COMP_DIR=""
+COPY_DIR=""
+
+HASH_ALGO=0
+OUTP_OPT=0
+DB_DELETE=0
+
+set_hash_algo () {
+
+    # Print actions
+    print_actions "UP/DN::Navigate"
+
+    # Print the title
+    print_title "SELECT HASH ALGORITHM"
+
+    await_to_proceed
+
+}
+
 config_loop () {
 
-    #...
+    clear_content
+
+    set_hash_algo
 
 }
