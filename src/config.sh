5a94a4325bde9a68d84b2823864a0653692de312
komed3 <pk97@outlook.de>
2025-07-05 00:10:10 +0200

    fix display error while toggle options

--- src/config.sh
+++ src/config.sh
@@ -13,20 +13,27 @@ HASH_ALGO=0
 OUTP_OPT=0
 DB_DELETE=0
 
+# Set the alorithm for hashing
 set_hash_algo () {
 
     # Print the title
     print_title "SELECT HASH ALGORITHM"
 
-    # Menu to select from available hashes
+    # Menu to select algorithm from available hashes
     select_menu 0 1 0 AVAILABLE_HASHES
+
+    # Save the selected hash algorithm
+    HASH_ALGO=${result[0]}
 
 }
 
+# Loop to configure the script
 config_loop () {
 
+    # Clear previous content
     clear_content
 
+    # Step 1: Set hash algorithm
     set_hash_algo
 
 }
--- src/utils/select.sh
+++ src/utils/select.sh
@@ -8,23 +8,35 @@ source "$SCRIPT_DIR/utils/ctrl.sh"
 # Default state to select first option
 SELECT_NULL=( 1 )
 
+# Function to display a selection menu
+# Arguments:
+#   $1: multi_flag (0 = Radio, 1 = Checkbox)
+#   $2: require_one (1 = at least one selection required)
+#   $3: allow_none (1 = no choice allowed, 0 = at least one for radio)
+#   $4: opts_name (Array name of the options)
+#   $5: init_name (Array name of initial states, default is SELECT_NULL)
+# Returns:
+#   result: Array of selected indices
 select_menu () {
 
     # Proceed inputs
-    local multi_flag="$1"               # 0 = Radio (single), 1 = Checkbox (multi)
-    local require_one="$2"              # 1 = at least one selection required
-    local allow_none="$3"               # 1 = no choice allowed, 0 = at least one for radio
-    local opts_name="$4"                # Array name of the options
-    local init_name="${5:-SELECT_NULL}" # Array name of initial states
+    local multi_flag="$1"
+    local require_one="$2"
+    local allow_none="$3"
+    local opts_name="$4"
+    local init_name="${5:-SELECT_NULL}"
 
     # Set refs to the given arrays
     local -n opts_ref="${opts_name}"
     local -n init_ref="${init_name}"
-    local -a selected                   # Selected indices
+    local -a selected
 
     # Define some variables to contol inputs
-    local count=${#opts_ref[@]}         # Number of options
-    local pointer=0 prev=-1             # Current / prev. cursor position
+    local count=${#opts_ref[@]}
+    local pointer=0
+
+    # The result array
+    result=()
 
     # Set initial states (default 0)
     for (( i=0; i < count; i++ )); do
@@ -41,32 +53,24 @@ select_menu () {
     # Show options list and proceed user inputs
     while true; do
 
-        # Render only if the pointer has changed
-        if (( pointer != prev )); then
+        # Jump to content
+        jump_content
 
-            # Jump to content
-            jump_content
+        # List all options
+        for i in "${!opts_ref[@]}"; do
 
-            # List all options
-            for i in "${!opts_ref[@]}"; do
+            hl=""; mark=" "
 
-                hl=""; mark=" "
+            # Highlight current line
+            if (( i == pointer )); then hl="$REVID"; fi
 
-                # Highlight current line
-                if (( i == pointer )); then hl="$REVID"; fi
+            # Mark "x" on selected option
+            if (( selected[$i] == 1 )); then mark="x"; fi
 
-                # Mark "x" on selected option
-                if (( selected[$i] == 1 )); then mark="x"; fi
+            # Print the option
+            printf "%s%s[%s] %s%s\n" "$PRFX" "$hl" "$mark" "${opts_ref[$i]}" "$RESET"
 
-                # Print the line
-                printf "%s%s[%s] %s%s\n" "$PRFX" "$hl" "$mark" "${opts_ref[$i]}" "$RESET"
-
-            done
-
-        fi
-
-        # Set previous pointer to current
-        prev=$pointer
+        done
 
         # Read key input
         read_key
@@ -85,7 +89,7 @@ select_menu () {
                 if (( multi_flag == 0 )); then
                     # Radio: deselect all, then activate the current one
                     for (( j=0; j < count; j++ )); do selected[$j]=0; done
-                    selected[$pointer]=1
+                    selected[$pointer]=1;
                 else
                     # Checkbox: only deselect if allowed
                     if (( selected[$pointer] == 1 )); then
@@ -110,6 +114,11 @@ select_menu () {
 
         esac
 
+    done
+
+    # Write result to array
+    for i in "${!selected[@]}"; do
+        (( selected[$i] == 1 )) && result+=( "$i" )
     done
 
 }
