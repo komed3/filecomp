4f632c0934aa7b2db757e0922b8141114e40d176
komed3 <pk97@outlook.de>
2025-07-04 23:46:15 +0200

    bug fix

--- src/utils/env.sh
+++ src/utils/env.sh
@@ -8,16 +8,16 @@ check_available_hashes () {
 
     # Map: Algorithm → Command
     declare -A hash_cmds=(
-        [ "SHA1" ]="sha1sum"
-        [ "SHA256" ]="sha256sum"
-        [ "SHA512" ]="sha512sum"
-        [ "MD5" ]="md5sum"
-        [ "B2" ]="b2sum"
+        [SHA1]="sha1sum"
+        [SHA256]="sha256sum"
+        [SHA512]="sha512sum"
+        [MD5]="md5sum"
+        [B2]="b2sum"
     )
 
     AVAILABLE_HASHES=()
 
-    for $name in "${!hash_cmds[@]}"; do
+    for name in "${!hash_cmds[@]}"; do
         if command -v "${hash_cmds[$name]}" >/dev/null 2>&1; then
             AVAILABLE_HASHES+=( "$name" )
         fi
