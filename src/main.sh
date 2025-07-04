f34f84f99540a36a89ddfcdda48d14cdbacd1fcc
komed3 <pk97@outlook.de>
2025-07-04 11:00:45 +0200

    check available hash algos

--- src/main.sh
+++ src/main.sh
@@ -2,12 +2,14 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+source "$SCRIPT_DIR/utils/env.sh"
 source "$SCRIPT_DIR/utils/ui.sh"
 
 source "$SCRIPT_DIR/help.sh"
 
 main () {
 
+    check_available_hashes
     setup_env
 
     print_header
